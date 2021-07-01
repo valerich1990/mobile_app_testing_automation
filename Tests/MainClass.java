e956db80b254e087b9553d83bdfeb2dddba329c6
valerich1990 <arelkov.kdavt@gmail.com>
2021-07-01 18:50:57 +0300

    Home work 3

--- Tests/MainClass.java
+++ Tests/MainClass.java
@@ -1,5 +1,11 @@
 public class MainClass
 {
+    private String class_string = "Hello, world";
+    public String getClassString()
+    {
+        return class_string;
+    }
+
     private int class_number = 20;
     public int getClassNumber()
     {
--- Tests/MainClassTest.java
+++ Tests/MainClassTest.java
@@ -8,9 +8,16 @@ public class MainClassTest extends MainClass
     {
         Assert.assertTrue("GetLocalNumber return not 12",getLocalNumber() == 12);
     }
+
     @Test
     public void testGetClassNumber()
     {
         Assert.assertTrue("GetClassNumber return < 45",getClassNumber() > 45);
+    }
+
+    @Test
+    public void testGetClassString()
+    {
+        Assert.assertTrue("GetClassString return hello or Hello",getClassString().contains("hello") || getClassString().contains("Hello"));
     }
 }
